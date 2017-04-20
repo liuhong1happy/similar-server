e35868e19c3159d0c83f5eeb4e9bb95d09f9f251
liuhong <liuhong@idealsee.cn>
2017-04-20 18:11:21 +0800

    update

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
     Application: require('./lib/application'),
     Route: require('./lib/route'),
-    Router: require('./lib/router')
+    Router: require('./lib/router'),
+    Controller: require('./lib/controller');
 }
--- template/index.js
+++ template/index.js
@@ -1,16 +1,22 @@
 const SimpleServer = require('similar-server');
-const { Application, Route, Router } = SimpleServer;
+const { Application, Route, Router, Controller } = SimpleServer;
 const app = SimpleServer.Application();
 
-const HomeIndexHandler = (req, res) => {
-
+class HomeIndexController extends Controller {
+    GET(req, res, next) {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('X-Foo', 'bar');
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.end('home-index');
+    }
 }
 
 app.router(Router('/',[
     Route('home', [
-        Route('index', HomeIndexHandler),
+        Route('index', new HomeIndexController()),
     ])   
 ]));
+
 app.init();
 
 app.listen(3002);
