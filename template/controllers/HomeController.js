e80067fe9c0ff1af8a0a2585c2eb0dc633e8b031
Holly Liu <hollyliu@HollydeMac-mini.local>
2017-04-22 19:01:40 +0800

    update

--- template/controllers/HomeController.js
+++ template/controllers/HomeController.js
@@ -1,10 +1,11 @@
 import Controller from 'similar-server/dist/controller';
 import { RenderView } from 'similar-server/dist/view';
+import HomeModel from '../models/HomeModel';
 
 class HomeController extends Controller {
     @RenderView('index.html')
     GET(req, res, next) {
-        return {};
+        return new HomeModel();
     }
 }
 
