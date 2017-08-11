d6da8ebcbfc2a968a14a33d4633bc6e495cbeeec
liuhong <liuhong@idealsee.cn>
2017-08-11 16:03:45 +0800

    修改为同步逻辑

--- dist/static.js
+++ dist/static.js
@@ -11,61 +11,60 @@ exports.default = function (assetsDir) {
         var realPath = _path2.default.join(assetsDir, _path2.default.normalize(pathname.replace(/\.\./g, "")));
         var pathHandle = function pathHandle(realPath) {
             //用fs.stat方法获取文件
-            _fs2.default.stat(realPath, function (err, stats) {
-                if (err) {
+            try {
+                var stats = _fs2.default.stat(realPath);
+                if (stats.isDirectory()) {
                     next();
                 } else {
-                    if (stats.isDirectory()) {
-                        next();
-                    } else {
-                        var ext = _path2.default.extname(realPath);
-                        ext = ext ? ext.slice(1) : 'unknown';
+                    var ext = _path2.default.extname(realPath);
+                    ext = ext ? ext.slice(1) : 'unknown';
 
-                        var contentType = _mime2.default.lookup(realPath) || "text/plain";
-                        response.setHeader("Content-Type", contentType);
+                    var contentType = _mime2.default.lookup(realPath) || "text/plain";
+                    response.setHeader("Content-Type", contentType + ";charset=utf-8");
 
-                        var lastModified = stats.mtime.toUTCString();
-                        var ifModifiedSince = "If-Modified-Since".toLowerCase();
-                        response.setHeader("Last-Modified", lastModified);
+                    var lastModified = stats.mtime.toUTCString();
+                    var ifModifiedSince = "If-Modified-Since".toLowerCase();
+                    response.setHeader("Last-Modified", lastModified);
 
-                        var Expires = {
-                            fileMatch: /^(gif|png|jpg|js|css)$/ig,
-                            maxAge: 60 * 60 * 24 * 365
-                        };
+                    var Expires = {
+                        fileMatch: /^(gif|png|jpg|js|css)$/ig,
+                        maxAge: 60 * 60 * 24 * 365
+                    };
 
-                        if (ext.match(Expires.fileMatch)) {
-                            var expires = new Date();
-                            expires.setTime(expires.getTime() + Expires.maxAge * 1000);
-                            response.setHeader("Expires", expires.toUTCString());
-                            response.setHeader("Cache-Control", "max-age=" + Expires.maxAge);
-                        }
-
-                        if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
-                            response.writeHead(304, "Not Modified");
-                            response.end();
-                        } else {
-                            var Compress = {
-                                match: /css|js|html/ig
-                            };
-                            var raw = _fs2.default.createReadStream(realPath);
-                            var acceptEncoding = request.headers['accept-encoding'] || "";
-                            var matched = ext.match(Compress.match);
-
-                            if (matched && acceptEncoding.match(/\bgzip\b/)) {
-                                response.writeHead(200, "Ok", { 'Content-Encoding': 'gzip' });
-                                raw.pipe(_zlib2.default.createGzip()).pipe(response);
-                            } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
-                                response.writeHead(200, "Ok", { 'Content-Encoding': 'deflate' });
-                                raw.pipe(_zlib2.default.createDeflate()).pipe(response);
-                            } else {
-                                response.writeHead(200, "Ok");
-                                raw.pipe(response);
-                            }
-                        }
-                        console.info(_colors2.default.yellow, '[similar-server][Static][' + new Date().toLocaleString() + '][' + realPath + ']');
+                    if (ext.match(Expires.fileMatch)) {
+                        var expires = new Date();
+                        expires.setTime(expires.getTime() + Expires.maxAge * 1000);
+                        response.setHeader("Expires", expires.toUTCString());
+                        response.setHeader("Cache-Control", "max-age=" + Expires.maxAge);
                     }
+
+                    if (request.headers[ifModifiedSince] && lastModified == request.headers[ifModifiedSince]) {
+                        response.writeHead(304, "Not Modified");
+                        response.end();
+                    } else {
+                        var Compress = {
+                            match: /css|js|html/ig
+                        };
+                        var raw = _fs2.default.createReadStream(realPath);
+                        var acceptEncoding = request.headers['accept-encoding'] || "";
+                        var matched = ext.match(Compress.match);
+
+                        if (matched && acceptEncoding.match(/\bgzip\b/)) {
+                            response.writeHead(200, "Ok", { 'Content-Encoding': 'gzip' });
+                            raw.pipe(_zlib2.default.createGzip()).pipe(response);
+                        } else if (matched && acceptEncoding.match(/\bdeflate\b/)) {
+                            response.writeHead(200, "Ok", { 'Content-Encoding': 'deflate' });
+                            raw.pipe(_zlib2.default.createDeflate()).pipe(response);
+                        } else {
+                            response.writeHead(200, "Ok");
+                            raw.pipe(response);
+                        }
+                    }
+                    console.info(_colors2.default.yellow, '[similar-server][Static][' + new Date().toLocaleString() + '][' + realPath + ']');
                 }
-            });
+            } catch (error) {
+                next();
+            }
         };
         pathHandle(realPath);
     };
